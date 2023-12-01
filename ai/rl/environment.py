c7ee6b1eb8676eb79d102eb86f950e852bf903cf
seb-sep <59621473+seb-sep@users.noreply.github.com>
2023-11-30 23:05:30 -0500

    init

---
+++ ai/rl/environment.py
@@ -0,0 +1,31 @@
+from collections import defaultdict
+from typing import Optional
+
+import numpy as np
+import torch
+import tqdm
+from tensordict.nn import TensorDictModule
+from tensordict.tensordict import TensorDict, TensorDictBase
+from torch import nn
+
+from torchrl.data import BoundedTensorSpec, CompositeSpec, UnboundedContinuousTensorSpec
+from torchrl.envs import (
+    CatTensors,
+    EnvBase,
+    Transform,
+    TransformedEnv,
+    UnsqueezeTransform,
+)
+from torchrl.envs.transforms.transforms import _apply_to_composite
+from torchrl.envs.utils import check_env_specs, step_mdp
+
+DEFAULT_X = np.pi
+DEFAULT_Y = 1.0
+
+def _step(observation: TensorDict) -> TensorDict:
+    '''
+    Execute the action in the tensordict, get the observations from the game,
+    and return the new observation.
+    '''
+
+    action = observation['action']
