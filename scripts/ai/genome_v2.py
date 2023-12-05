58a579219927c0d288e06fef5e2a39fabe855d16
seb-sep <59621473+seb-sep@users.noreply.github.com>
2023-12-04 20:19:40 -0500

    feed forward stuff

--- scripts/ai/genome_v2.py
+++ scripts/ai/genome_v2.py
@@ -1,3 +1,14 @@
+import numpy as np
 
-def feed_forward(inputs):
-    pass
+def feed_forward(inputs: list):
+    inputs = [1] + inputs # add bias
+    inputs = np.array(inputs)
+    n = len(inputs)
+    l1 = np.random.rand(n, 3)
+    l2 = np.random.rand(3, 2)
+
+    x = np.dot(inputs, l1)
+    x = np.expit(x)
+    x = np.dot(x, l2)
+    x = np.expit(x)
+    return np.argmax(x) # 0 for no lift, 1 for lift
