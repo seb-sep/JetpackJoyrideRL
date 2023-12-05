3544eabdba56fb59a64ac78d7906776c6e6430a2
seb-sep <59621473+seb-sep@users.noreply.github.com>
2023-12-04 20:22:10 -0500

    refined forward pass

--- scripts/ai/genome_v2.py
+++ scripts/ai/genome_v2.py
@@ -7,8 +7,5 @@ def feed_forward(inputs: list):
     l1 = np.random.rand(n, 3)
     l2 = np.random.rand(3, 2)
 
-    x = np.dot(inputs, l1)
-    x = np.expit(x)
-    x = np.dot(x, l2)
-    x = np.expit(x)
+    x = np.expit(np.expit(inputs.dot(l1)).dot(l2))
     return np.argmax(x) # 0 for no lift, 1 for lift
